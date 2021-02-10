70c3c37f147210e12fd52ba85dd0b9c05b248162
Alison Ghedin <alison.ghedin@ambevtech.com.br>
2021-02-09 22:47:09 -0300

    finalizando mini-curso node

--- first-app/app_event2.js
+++ first-app/app_event2.js
@@ -1,20 +1,14 @@
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
 // Register a listener
 // emitter.addListener é o mesmo comando que emitter.on
 
-emitter.on('messageLogged', (arg) => {
+
+const Logger = require('./logger')
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
     console.log('Listener called', arg);
 })
 
-
-emitter.on('loging', (arg) => {
-    console.log('logando');
-})
-
-
-// emit: making a noise produce - sinalizando que algo aconteceu
-// Raise an event
-emitter.emit('messageLogged', {id: 1, url: 'http://'});
-emitter.emit('loging');
+logger.log('mensagem principal');
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,12 +1,18 @@
+const EventEmitter = require('events');
+
 const url = 'http://mylogger.io/log';
 
-function log(message){
-    //Send a HTTP request
-    console.log(message);
+class Logger extends EventEmitter{
+    log(message){
+        //Send a HTTP request
+        console.log(message);
+        // Raise an event
+        this.emit('messageLogged', {id: 1, url: 'http://'});
+    }
+    
 }
-
 
 //se o modulo possuir só uma função/variavel/objeto que queira exportar, 
 // utilizar só module.exports = object
-module.exports = log;
+module.exports = Logger;
 //module.exports.endPoint = url;
